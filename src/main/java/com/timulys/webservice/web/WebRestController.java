5df89f9a04cb64b224a6de6c33b97313faae8765
timulys <timulys@gmail.com>
2020-09-21 16:11:13 +0900

    3-2. 게시글 등록 전까지 완료

--- src/main/java/com/timulys/webservice/web/WebRestController.java
+++ src/main/java/com/timulys/webservice/web/WebRestController.java
@@ -1,12 +1,26 @@
 package com.timulys.webservice.web;
 
+import com.timulys.webservice.domain.posts.PostsRepository;
+import com.timulys.webservice.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld!";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
